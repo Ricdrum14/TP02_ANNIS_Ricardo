eb1811a1d562f14c6a5c3a0385e85ef307fc1d1a
Ricardo <ricardo.annis@etu.unistra.fr>
2024-10-01 22:17:38 +0200

    Renommé le dossier TP2 en TP02_ANNIS_Ricardo et mis à jour app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,3 +11,9 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'TP2';
 }
+
+export class FormComponent{
+
+  nom : String = "Toto";
+  valid : boolean = true;
+}
